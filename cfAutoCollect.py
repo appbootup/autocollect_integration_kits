b36930fc385a42dc061687ffbe166a000d24851b
preetha-in <40459012+preetha-in@users.noreply.github.com>
2018-06-26 16:08:18 +0530

    Added Python AutoCollect Integration Code

--- cfAutoCollect.py
+++ cfAutoCollect.py
@@ -53,8 +53,7 @@ class cfAutoCollect:
 
             cfAutoCollect.token = jsonData["data"]["token"]
             cfAutoCollect.expiry = jsonData["data"]["expiry"]
-
-em            return cfAutoCollect.token
+            return cfAutoCollect.token
 
         except Exception as e:
             return e 
@@ -139,31 +138,3 @@ em            return cfAutoCollect.token
             return False
 
 
-
-           
-
-exasmple = cfAutoCollect("CF27EFXRWU3WKIAAUUQ", "5bd9ba5e0bf69112dde1a7f0a77121c3b78b144c", "TEST")
-print exasmple.client_auth()
-print exasmple.list_all_va()
-#dummy data for webhook execution
-
-data = {
-     "event" :    "AMOUNT_COLLECTED",
-     "amount" : "15",
-     "vAccountId" : "PREE",
-     "vAccountNumber"  : "CASHFREELGSHPREE",
-     "email" : "preetha.datta@gmail.com",
-     "phone"  : "9910115208",
-    "referenceId"   : "165196",
-    "utr" : "78956755343816",
-    "creditRefNo" : "9240092284977434",
-    "remitterAccount" : "1234567890",
-    "remitterName" : "Cashfree",
-    "paymentTime": "2018-06-25 18:02:37",
-    "signature"   :  "8jT8J7nER2jExS55K0Gp3EgT2QP7kVNw6lXC0doanf8="
-}
-#receive data through our webhook calls.
-print exasmple.is_valid_signature(data)
-
-
-        
