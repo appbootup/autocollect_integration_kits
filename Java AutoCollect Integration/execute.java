6d21e719ee67bee94c2944deca59030812a9f065
preetha-in <40459012+preetha-in@users.noreply.github.com>
2018-06-27 12:10:12 +0530

    Update execute.java

--- Java AutoCollect Integration/execute.java
+++ Java AutoCollect Integration/execute.java
@@ -5,7 +5,7 @@ public class execute{
 
     public static void main(String[] args)throws Exception {
 
-        cfAutoCollect userExample = new cfAutoCollect("dummyclientId", "dummyClientSecret", "TEST");
+        cfAutoCollect userExample = new cfAutoCollect("dummyclientId", "dummyClientSecret", "TEST/PROD");
 
         System.out.println(userExample.client_auth());
         userExample.create_virtual_account("TEST","Tester", "9999999999", "tester@gmail.com");
